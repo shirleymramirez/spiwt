32169f209ce23d6d93dbdbd92da3a86268f9a0a6
shirley <shirley.mramirez@yahoo.com>
2019-02-27 10:41:15 -0700

    modified product.js and release setup

--- src/const/products.js
+++ src/const/products.js
@@ -1,7 +1,11 @@
 const PRODUCTS = {
     'DWS-90': {
         imageURL: 'https://www.spectrapure.com/wp-content/uploads/2018/12/DWS-90-1.png',
-        description: 'DWS 90 GPD RO Drinking Water System, 4 gal Pressurized Storage Tank with Air Gap Faucet',
+        description: 'Drinking Water System with 4 Gallon Tank and Air Gap Faucet',
+    },
+    'UHE-200': {
+        imageURL: 'https://www.spectrapure.com/wp-content/uploads/2018/12/MC-RODI-200UHE-10-600x600.png',
+        description: 'MaxCap® 1:1 Ultra High Efficiency 200 GPD RO/DI System – MC-RODI'
     }
 }
 
