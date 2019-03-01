c3b538255fec6c0ce1a80d51cfcc764ea01710d9
shirley <shirley.mramirez@yahoo.com>
2019-03-01 09:08:54 -0700

    update link

--- src/const/products.js
+++ src/const/products.js
@@ -2,12 +2,12 @@ const PRODUCTS = {
   'DWS-90': {
     imageURL: 'https://www.spectrapure.com/wp-content/uploads/2018/12/DWS-90-1.png',
     description: 'Drinking Water System with 4 Gallon Tank and Air Gap Faucet',
-    userManualURL: 'https://spectrapure.com/product-manuals/MAXCAPDI-D1-Systems-Add-On-DI-Kits.pdf',
+    userManualURL: 'https://www.spectrapure.com/product-manuals/Drinking%20Water%20Systems/Drinking-Water-Systems.pdf',
   },
   'UHE-200': {
     imageURL: 'https://www.spectrapure.com/wp-content/uploads/2018/12/MC-RODI-200UHE-10-600x600.png',
     description: 'MaxCap® 1:1 Ultra High Efficiency 200 GPD RO/DI System',
-    userManualURL: 'https://spectrapure.com/product-manuals/MAXCAPDI-D1-Systems-Add-On-DI-Kits.pdf',
+    userManualURL: 'https://www.spectrapure.com/product-manuals/Ultra-High%20Efficiency%20Systems/MC-RODI-200UHE.pdf',
   },
 }
 
